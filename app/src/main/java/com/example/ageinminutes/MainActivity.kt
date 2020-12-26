197f86ca77bd6986b3e555d633763eae30b58d36
simonw101a <simonw101@googlemail.com>
2020-12-26 18:11:33 +0000

    lecture 78

--- app/src/main/java/com/example/ageinminutes/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutes/MainActivity.kt
@@ -7,17 +7,19 @@ import android.view.View
 import android.widget.Button
 import android.widget.Toast
 import com.example.ageinminutes.databinding.ActivityMainBinding
+import java.text.SimpleDateFormat
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        var button = findViewById<Button>(R.id.btnDatePicker)
-
-        button.setOnClickListener {view ->
+        binding.btnDatePicker.setOnClickListener {view ->
 
             clickDatePicker(view)
 
@@ -35,9 +37,17 @@ class MainActivity : AppCompatActivity() {
 
         val day = myCalender.get(Calendar.DAY_OF_MONTH)
 
-        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, year, month, dayOfMonth ->
+        DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view, selectedYear, selectedMonth, selectedDayOfMonth ->
 
-            Toast.makeText(this, "works", Toast.LENGTH_LONG).show()
+            Toast.makeText(this, "The chosen date is $selectedDayOfMonth/${selectedMonth+1}/$selectedYear", Toast.LENGTH_LONG).show()
+
+            val selectedDate = "$selectedDayOfMonth/${selectedMonth+1}/$selectedYear"
+
+            binding.selectedDate.text = selectedDate
+
+            val sdf = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+
+            val theDate = sdf.parse(selectedDate)
 
         }, year, month, day).show()
 
